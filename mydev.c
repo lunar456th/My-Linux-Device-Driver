803d964fd9fe0bcce0e26c53b84f62311458381c
lunar456th <lunar456th@naver.com>
2019-08-25 15:52:02 +0900

    read write ioctl

--- mydev.c
+++ mydev.c
@@ -12,15 +12,25 @@
 #define DEV_MINOR_NUM 242
 #define DEV_NAME "mydev"
 
-#define BUFF_SIZE 1024
+#define BUFFER_SIZE 256
 
-static int	my_open(struct inode *, struct file *);
-static int	my_release(struct inode *, struct file *);
+struct data {
+	unsigned char buffer[BUFFER_SIZE];
+};
+
+static int		my_open(struct inode *, struct file *);
+static int		my_release(struct inode *, struct file *);
+static ssize_t	my_read(struct file *, char *, size_t, loff_t *);
+static ssize_t	my_write(struct file *, const char *, size_t, loff_t *);
+static long		my_ioctl(struct file *, unsigned int, unsigned long);
 
 static struct file_operations my_fops = {
-	.owner		= THIS_MODULE,
-	.open		= my_open,
-	.release	= my_release,
+	.owner			= THIS_MODULE,
+	.open			= my_open,
+	.release		= my_release,
+	.read			= my_read,
+	.write			= my_write,
+	.unlocked_ioctl = my_ioctl,
 };
 
 static struct miscdevice my_misc = {
@@ -29,18 +39,6 @@ static struct miscdevice my_misc = {
 	.fops		= &my_fops,
 	.mode		= 0777,
 };
-
-static int my_open(struct inode *inode, struct file *filp)
-{
-	pr_info("[%s] %s()\n", __FILE__, __FUNCTION__);
-	return 0;
-}
-
-static int my_release(struct inode *inode, struct file *filp)
-{
-	pr_info("[%s] %s()\n", __FILE__, __FUNCTION__);
-	return 0;
-}
 
 static int __init my_init(void)
 {
@@ -58,6 +56,121 @@ void __exit my_exit(void)
 {
 	pr_info("[%s] %s()\n", __FILE__, __FUNCTION__);
 	misc_deregister(&my_misc);
+}
+
+static int my_open(struct inode *inode, struct file *filp)
+{
+	char *str = "null";
+	int ret = 0;
+	struct data *p = kmalloc(sizeof(struct data), GFP_KERNEL);
+	pr_info("[%s] %s()\n", __FILE__, __FUNCTION__);
+	if (!p)
+	{
+		pr_info("[%s] %s():%d kmalloc null (%d)\n", __FILE__, __FUNCTION__, __LINE__, ret);
+		return -ENOMEM;
+	}
+	ret = strlcpy(p->buffer, str, sizeof(p->buffer));
+	if (ret > strlen(str))
+	{
+		pr_info("[%s] %s():%d strlcpy too long (%d)\n", __FILE__, __FUNCTION__, __LINE__, ret);
+	}
+	filp->private_data = p;
+	return 0;
+}
+
+static int my_release(struct inode *inode, struct file *filp)
+{
+	pr_info("[%s] %s()\n", __FILE__, __FUNCTION__);
+	if (filp->private_data)
+	{
+		kfree(filp->private_data);
+		filp->private_data = NULL;
+	}
+	return 0;
+}
+
+static ssize_t my_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
+{
+	int ret = 0;
+	struct data *p = filp->private_data;
+	pr_info("[%s] %s()\n", __FILE__, __FUNCTION__);
+	count = (count > BUFFER_SIZE) ? BUFFER_SIZE : count;
+	if ((ret = copy_to_user(buf, p->buffer, count)) < 0)
+	{
+		pr_info("[%s] %s():%d copy_to_user failed (%d)\n", __FILE__, __FUNCTION__, __LINE__, ret);
+		return -EFAULT;
+	}
+	return count;
+}
+
+static ssize_t my_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
+{
+	int ret = 0;
+	struct data *p = filp->private_data;
+	pr_info("[%s] %s()\n", __FILE__, __FUNCTION__);
+	pr_info("[%s] %s() copy_from_user() Before (0x%p,%s)\n", __FILE__, __FUNCTION__, p->buffer, p->buffer);
+	if ((ret = copy_from_user(p->buffer, buf, count)) < 0)
+	{
+		pr_info("[%s] %s():%d copy_from_user failed (%d)\n", __FILE__, __FUNCTION__, __LINE__, ret);
+		return -EFAULT;
+	}
+	pr_info("[%s] %s() copy_from_user() After  (0x%p,%s)\n", __FILE__, __FUNCTION__, p->buffer, p->buffer);
+	return count;
+}
+
+static long my_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	int ret = 0, size = 0;
+	pr_info("[%s] %s()\n", __FILE__, __FUNCTION__);
+	pr_info("[%s] %s() cmd:%u\n", __FILE__, __FUNCTION__, cmd);
+	if (_IOC_TYPE(cmd) != MYDEV_IOCTL_MAGIC)
+	{
+		pr_info("[%s] %s():%d invalid magic number\n", __FILE__, __FUNCTION__, __LINE__);
+		return -EINVAL;
+	}
+	if (_IOC_NR(cmd) >= MYDEV_IOCTL_MAX_NR)
+	{
+		pr_info("[%s] %s():%d exceeded max value of cmd\n", __FILE__, __FUNCTION__, __LINE__);
+		return -EINVAL;
+	}
+	if ((size = _IOC_SIZE(cmd)))
+	{
+		if (_IOC_DIR(cmd) & _IOC_READ)
+		{
+			ret = !access_ok(VERIFY_WRITE, (void __user*)arg, size);
+		}
+		else if (_IOC_DIR(cmd) & _IOC_WRITE)
+		{
+			ret = !access_ok(VERIFY_READ, (void __user*)arg, size);
+		}
+		if (ret)
+		{
+			return ret;
+		}
+	}
+	switch (cmd)
+	{
+		case MYDEV_IOCTL_INIT:
+			pr_info("[%s] %s() MYDEV_IOCTL_INIT\n", __FILE__, __FUNCTION__);
+			break;
+		case MYDEV_IOCTL_DEINIT:
+			pr_info("[%s] %s() MYDEV_IOCTL_DEINIT\n", __FILE__, __FUNCTION__);
+			break;
+		case MYDEV_IOCTL_READ:
+			pr_info("[%s] %s() MYDEV_IOCTL_READ\n", __FILE__, __FUNCTION__);
+			break;
+		case MYDEV_IOCTL_WRITE:
+			pr_info("[%s] %s() MYDEV_IOCTL_WRITE\n", __FILE__, __FUNCTION__);
+			break;
+		case MYDEV_IOCTL_RDWR:
+			pr_info("[%s] %s() MYDEV_IOCTL_RDWR\n", __FILE__, __FUNCTION__);
+			break;
+		default:
+			pr_info("[%s] %s():%d unregistered cmd\n", __FILE__, __FUNCTION__, __LINE__);
+			ret = -EINVAL;
+			break;
+	}
+	return ret;
 }
 
 module_init(my_init);
--- test_mydev.c
+++ test_mydev.c
@@ -1,22 +1,68 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/ioctl.h>
-
 #include "mydev.h"
 
-int main(void)
+#define TEXT_LEN 14
+
+int main()
 {
+	static char buff[256];
 	int fd = 0, ret = 0;
 
-	if(fd = open("/dev/mydev", O_RDWR) < 0)
+	// open
+	if ((fd = open("/dev/mydev", O_RDWR)) < 0)
 	{
-		perror("open failed\n");
+		fprintf(stderr, "open failed (%d)\n", fd);
 		return -1;
 	}
 
-	close(fd);
+	// write
+	if ((ret = write(fd, "Hello World!", TEXT_LEN)) < 0)
+	{
+		fprintf(stderr, "write failed (%d)\n", ret);
+	}
+
+	// read
+	if ((ret = read(fd, buff, TEXT_LEN)) < 0)
+	{
+		fprintf(stderr, "read failed (%d)\n", ret);
+	}
+	else
+	{
+		printf("read -> (ret:%d,buff:%s)\n", ret, buff);
+	}
+
+	// ioctl
+	if ((ret = ioctl(fd, MYDEV_IOCTL_INIT)) < 0)
+	{
+		fprintf(stderr, "ioctl MYDEV_IOCTL_INIT failed (%d)\n", ret);
+	}
+	if ((ret = ioctl(fd, MYDEV_IOCTL_DEINIT)) < 0)
+	{
+		fprintf(stderr, "ioctl MYDEV_IOCTL_DEINIT failed (%d)\n", ret);
+	}
+	if ((ret = ioctl(fd, MYDEV_IOCTL_READ)) < 0)
+	{
+		fprintf(stderr, "ioctl MYDEV_IOCTL_READ failed (%d)\n", ret);
+	}
+	if ((ret = ioctl(fd, MYDEV_IOCTL_WRITE)) < 0)
+	{
+		fprintf(stderr, "ioctl MYDEV_IOCTL_WRITE failed (%d)\n", ret);
+	}
+	if ((ret = ioctl(fd, MYDEV_IOCTL_RDWR)) < 0)
+	{
+		fprintf(stderr, "ioctl MYDEV_IOCTL_RDWR failed (%d)\n", ret);
+	}
+
+	// release
+	if ((ret = close(fd)) < 0)
+	{
+		fprintf(stderr, "close failed (%d)\n", ret);
+	}
 
 	return 0;
 }
